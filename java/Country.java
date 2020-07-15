9d8e381f798455f590e0670895370bb69caebd61
Marco Angel <angel2367716@gmail.com>
2020-07-14 18:03:25 -0700

    first java project with ecplise

--- java/Country.java
+++ java/Country.java
@@ -2,4 +2,8 @@ class Country {
     void comingSoon(){
         System.out.println("War never changes");
     }
+    public static void main(String[] arg){
+        Country japan = new Country();
+        japan.comingSoon();
+    }
 }
