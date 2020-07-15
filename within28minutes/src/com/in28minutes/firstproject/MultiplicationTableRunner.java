c9667905928ccde844619c18f7445b2ece3395a7
Marco Angel <angel2367716@gmail.com>
2020-07-15 08:16:36 -0700

    refactor multiplication table

--- within28minutes/src/com/in28minutes/firstproject/MultiplicationTableRunner.java
+++ within28minutes/src/com/in28minutes/firstproject/MultiplicationTableRunner.java
@@ -5,10 +5,10 @@ public class MultiplicationTableRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MultiplicationTable table = new MultiplicationTable();
-		//table.print();
-		table.print(5);
-		table.print(6);
-		table.print(18);
+		table.print();
+		// table.print(5);
+		// table.print(6);
+		// table.print(18);
 	}
 
 }
