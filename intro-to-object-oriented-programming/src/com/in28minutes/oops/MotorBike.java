755512178844ad9a52b98ede555144cfeaca0f91
Marco Angel <angel2367716@gmail.com>
2020-07-15 18:47:02 -0700

    added a private variable and created a method to keep encapsulation on the motorbike class

--- intro-to-object-oriented-programming/src/com/in28minutes/oops/MotorBike.java
+++ intro-to-object-oriented-programming/src/com/in28minutes/oops/MotorBike.java
@@ -2,9 +2,22 @@ package com.in28minutes.oops;
 
 public class MotorBike {
 	// state
-	int speed;
+	private int speed; // member variable
 
 	// behavior
+
+	// method
+	// inputs - int speed
+	// outputs - void
+	// name - setSpeed
+
+	void setSpeed(int speed) { // local variable
+		this.speed = speed;
+
+		System.out.println(speed);
+		System.out.println(this.speed);
+
+	}
 
 	void start() {
 		System.out.println("bike started");
