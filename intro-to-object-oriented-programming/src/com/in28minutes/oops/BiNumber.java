81116917aa6c3efd85962076bddd6e716eb024af
Marco Angel <angel2367716@gmail.com>
2020-07-16 20:29:11 -0700

    created two new classes

---
+++ intro-to-object-oriented-programming/src/com/in28minutes/oops/BiNumber.java
@@ -0,0 +1,23 @@
+package com.in28minutes.oops;
+
+public class BiNumber {
+	private int bacon;
+	//	private int bacon2;
+
+	BiNumber(int bacon) {
+		this.bacon = bacon;
+	}
+
+	public int add() {
+		//		return bacon + bacon2;
+		return bacon;
+	}
+
+	public int multiply() {
+		return bacon;
+	}
+
+	//	void double() {
+	//		System.out.println("double method");
+	//	}
+}
