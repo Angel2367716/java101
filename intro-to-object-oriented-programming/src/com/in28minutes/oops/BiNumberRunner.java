97c64fb28a03859ea77142bf216a41022ed05843
Marco Angel <angel2367716@gmail.com>
2020-07-17 08:38:27 -0700

    created a new java project with a constructor. On this example, methods were auto generated with the cntrl + 1 command

--- intro-to-object-oriented-programming/src/com/in28minutes/oops/BiNumberRunner.java
+++ intro-to-object-oriented-programming/src/com/in28minutes/oops/BiNumberRunner.java
@@ -5,10 +5,10 @@ public class BiNumberRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		BiNumber numbers = new BiNumber(2);
-
-		System.out.println(numbers.add());
-		System.out.println(numbers.multiply());
+		//		BiNumber numbers = new BiNumber(2);
+		//
+		//		System.out.println(numbers.add());
+		//		System.out.println(numbers.multiply());
 
 
 	}
