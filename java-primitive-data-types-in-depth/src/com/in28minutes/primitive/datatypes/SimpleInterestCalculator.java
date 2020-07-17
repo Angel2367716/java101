aeafd934b43395551db144e21482b30e6c90dae0
Marco Angel <angel2367716@gmail.com>
2020-07-17 13:37:05 -0700

    creted 2 backup jshell files. Text format

---
+++ java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculator.java
@@ -0,0 +1,15 @@
+package com.in28minutes.primitive.datatypes;
+
+import java.math.BigDecimal;
+
+public class SimpleInterestCalculator {
+
+	public SimpleInterestCalculator(String string, String string2) {
+	}
+
+	public BigDecimal calculateTotalValue(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
