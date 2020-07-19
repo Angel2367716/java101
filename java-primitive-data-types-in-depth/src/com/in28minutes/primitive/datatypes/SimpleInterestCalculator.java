76cab04812521a55b9bc9e48c5d1fc48e8b8baab
Marco Angel <angel2367716@gmail.com>
2020-07-18 22:07:59 -0700

    updated simpleinterest calculator class

--- java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculator.java
+++ java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculator.java
@@ -3,13 +3,16 @@ package com.in28minutes.primitive.datatypes;
 import java.math.BigDecimal;
 
 public class SimpleInterestCalculator {
+	BigDecimal string;
+	BigDecimal string2;
 
 	public SimpleInterestCalculator(String string, String string2) {
+		this.string = new BigDecimal(string);
+		this.string2 = new BigDecimal(string2).divide(new BigDecimal(100));
 	}
 
 	public BigDecimal calculateTotalValue(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		BigDecimal i = BigDecimal(string);
 	}
 
 }
