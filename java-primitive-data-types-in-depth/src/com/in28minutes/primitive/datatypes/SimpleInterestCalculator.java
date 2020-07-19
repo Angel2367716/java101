db2029068f43878326a41ed3c5e569db76fe3b9f
Marco Angel <angel2367716@gmail.com>
2020-07-19 08:40:35 -0700

    Update on simpleInterestCalculator class
    The simple interest calculator class now accepts 2 variables to
    calculate. Using Big Decimals, we were able to create the correct
    methods to follow the interest formula:
    
    Total Value = principaal + (principal * interest * noOfYears);

--- java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculator.java
+++ java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculator.java
@@ -3,16 +3,20 @@ package com.in28minutes.primitive.datatypes;
 import java.math.BigDecimal;
 
 public class SimpleInterestCalculator {
-	BigDecimal string;
-	BigDecimal string2;
+	BigDecimal principal;
+	BigDecimal interest;
 
-	public SimpleInterestCalculator(String string, String string2) {
-		this.string = new BigDecimal(string);
-		this.string2 = new BigDecimal(string2).divide(new BigDecimal(100));
+	public SimpleInterestCalculator(String principal, String interest) {
+		this.principal = new BigDecimal(principal);
+		this.interest = new BigDecimal(interest).divide(new BigDecimal(100));
 	}
 
-	public BigDecimal calculateTotalValue(int i) {
-		BigDecimal i = BigDecimal(string);
+	public BigDecimal calculateTotalValue(int noOfYears) {
+		// Total value = principal + (principal * interest * nuOfYears)
+		BigDecimal noOfYearsBigDecimal = new BigDecimal(noOfYears);
+		BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(noOfYearsBigDecimal));
+		return totalValue;
 	}
+
 
 }
--- java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculatorRunner.java
+++ java-primitive-data-types-in-depth/src/com/in28minutes/primitive/datatypes/SimpleInterestCalculatorRunner.java
@@ -5,11 +5,11 @@ import java.math.BigDecimal;
 public class SimpleInterestCalculatorRunner {
 
 	public static void main(String[] args) {
-		SimpleInterestCalculator calculator = new SimpleInterestCalculator("45000.00", "7.5");
+		SimpleInterestCalculator calculator = new SimpleInterestCalculator("4500.00", "7.5");
 
 		BigDecimal totalValue = calculator.calculateTotalValue(5);// 5 years
 
-		System.out.println(totalSum);
+		System.out.println(totalValue);
 	}
 
 }
