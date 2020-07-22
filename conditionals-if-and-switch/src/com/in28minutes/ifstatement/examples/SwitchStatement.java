90ed42e821091f5e803639840d6a66f69d921f02
Marco Angel <angel2367716@gmail.com>
2020-07-21 19:41:44 -0700

    created a new method to track number of month

--- conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/SwitchStatement.java
+++ conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/SwitchStatement.java
@@ -4,6 +4,7 @@ public class SwitchStatement {
 
 	public static void main(String[] args) {
 		System.out.println(determineNameOfDay(5));
+		System.out.println(determineNameOfMoth(11));
 	}
 
 	public static String determineNameOfDay(int dayNumber) {
@@ -24,6 +25,37 @@ public class SwitchStatement {
 			return "Saturday";
 		}
 		return "Invvalid_day";
+	}
+
+	private static String determineNameOfMoth(int monthNumber) {
+		switch (monthNumber) {
+		case 1:
+			return "January";
+		case 2:
+			return "February";
+		case 3:
+			return "March";
+		case 4:
+			return "April";
+		case 5:
+			return "May";
+		case 6:
+			return "June";
+		case 7:
+			return "July";
+		case 8:
+			return "August";
+		case 9:
+			return "September";
+		case 10:
+			return "October";
+		case 11:
+			return "November";
+		case 12:
+			return "December";
+
+		}
+		return "Invalid_Month";
 	}
 
 }
