c1eb80adeb0312f9f2519adcb1ea5e1592983646
Marco Angel <angel2367716@gmail.com>
2020-07-21 19:40:42 -0700

    refactored determine name of day method

--- conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/SwitchStatement.java
+++ conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/SwitchStatement.java
@@ -6,33 +6,24 @@ public class SwitchStatement {
 		System.out.println(determineNameOfDay(5));
 	}
 
-		public static String determineNameOfDay(int dayNumber) {
-			String result = "";
-			switch (dayNumber) {
-			case 0:
-				result = "Sunday";
-				break;
-			case 1:
-				result = "Monday";
-				break;
-			case 2:
-				result = "Tuesday";
-				break;
-			case 3:
-				result = "Wednesday";
-				break;
-			case 4:
-				result = "Thursday";
-				break;
-			case 5:
-				result = "Friday";
-				break;
-			case 6:
-				result = "Saturday";
-				break;
-	
-			}
-			return result;
+	public static String determineNameOfDay(int dayNumber) {
+		switch (dayNumber) {
+		case 0:
+			return "Sunday";
+		case 1:
+			return "Monday";
+		case 2:
+			return "Tuesday";
+		case 3:
+			return "Wednesday";
+		case 4:
+			return "Thursday";
+		case 5:
+			return "Friday";
+		case 6:
+			return "Saturday";
 		}
+		return "Invvalid_day";
+	}
 
 }
