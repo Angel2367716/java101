5b834f3548c6ce7ca649af29bdb7beec20ba55d7
Marco Angel <angel2367716@gmail.com>
2020-07-23 08:40:29 -0700

    crated a while number class to pracitce while loops with java.

---
+++ loops/src/com/in28minutes/loops/WhileNumberPlayer.java
@@ -0,0 +1,26 @@
+package com.in28minutes.loops;
+
+public class WhileNumberPlayer {
+	private int limit;
+
+	public WhileNumberPlayer(int limit) {
+		this.limit = limit;
+
+	}
+
+	// for limit = 30, output would be 1 4 9 16 25
+	public void printSquaresUptoLimit() {
+		int i = 1;
+		while (i * i < 30) {
+			System.out.print(i * i + " ");
+			i++;
+		}
+
+	}
+
+	// For limit = 30, output would be 1 8 27
+	//	public void printSCubesUptoLimit() {
+	//
+	//	}
+
+}
